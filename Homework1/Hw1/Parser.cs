b01ac9dd7f02d5f2515553810f541dce6dab86a1
scilightener <nikita.l.boss@gmail.com>
2022-09-12 15:05:59 +0300

    [add] hw1 done

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -2,18 +2,34 @@ namespace Hw1;
 
 public static class Parser
 {
-    public static void ParseCalcArguments(string[] args, 
-        out double val1, 
-        out CalculatorOperation operation, 
+    public static void ParseCalcArguments(string[] args,
+        out double val1,
+        out CalculatorOperation operation,
         out double val2)
     {
-        throw new NotImplementedException();
+        if (!IsArgLengthSupported(args))
+            throw new ArgumentException("The number of arguments is not 3");
+
+        if (!double.TryParse(args[0], out val1))
+            throw new ArgumentException("Argument 1 is not a number");
+        if (!double.TryParse(args[2], out val2))
+            throw new ArgumentException("Argument 2 is not a number");
+        operation = ParseOperation(args[1]);
+        if (operation is CalculatorOperation.Undefined)
+            throw new InvalidOperationException("Unsupported operation");
     }
 
-    private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
+    private static bool IsArgLengthSupported(IReadOnlyCollection<string> args) => args.Count == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        return arg switch
+        {
+            "+" => CalculatorOperation.Plus,
+            "-" => CalculatorOperation.Minus,
+            "*" => CalculatorOperation.Multiply,
+            "/" => CalculatorOperation.Divide,
+            _ => CalculatorOperation.Undefined
+        };
     }
 }
