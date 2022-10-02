fd036c5a8b5ff97c073126bd2a91649a3580e2e4
scilightener <nikita.l.boss@gmail.com>
2022-10-02 15:18:12 +0300

    increase codecov percentage

--- Homework3/Hw3.Mutex/Program.cs
+++ Homework3/Hw3.Mutex/Program.cs
@@ -1,8 +1,17 @@
 ﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.Serialization;
 using Hw3.Mutex;
 
-Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {Process.GetCurrentProcess().Id} starts");
-using var wm = new WithMutex();
-Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {Process.GetCurrentProcess().Id} acquires mutex");
-Console.WriteLine(wm.Increment());
-Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {Process.GetCurrentProcess().Id} releases mutex");
+public class Program
+{
+    [ExcludeFromCodeCoverage]
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {Process.GetCurrentProcess().Id} starts");
+        using var wm = new WithMutex();
+        Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {Process.GetCurrentProcess().Id} acquires mutex");
+        Console.WriteLine(wm.Increment());
+        Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {Process.GetCurrentProcess().Id} releases mutex");
+    }
+}
