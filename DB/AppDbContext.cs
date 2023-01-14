eedd51246c536d0c411d2c07c4dcb2769b0d96bd
scilightener <nikita.l.boss@gmail.com>
2023-01-14 11:00:32 +0300

    [add] monsters in db

--- DB/AppDbContext.cs
+++ DB/AppDbContext.cs
@@ -6,6 +6,59 @@ namespace DB;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Monster> Monsters { get; set; } = null!;
     
-    public DbSet<Monster> Monsters { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var mimic = new Monster
+        {
+            Id = 1,
+            Name = "Mimic",
+            HitPoints = 58,
+            AttackModifier = 18,
+            AttackPerRound = 1,
+            Damage = "9d8",
+            DamageModifier = 3,
+            ArmorClass = 12
+        };
+
+        var arasta = new Monster
+        {
+            Id = 2,
+            Name = "Arasta",
+            HitPoints = 300,
+            AttackModifier = 144,
+            AttackPerRound = 1,
+            Damage = "24d12",
+            DamageModifier = 7,
+            ArmorClass = 19
+        };
+
+        var bugbearChief = new Monster
+        {
+            Id = 3,
+            Name = "Bugbear Chief",
+            HitPoints = 65,
+            AttackModifier = 20,
+            AttackPerRound = 1,
+            Damage = "10d8",
+            DamageModifier = 3,
+            ArmorClass = 17
+        };
+
+        var agony = new Monster
+        {
+            Id = 4,
+            Name = "Agony",
+            HitPoints = 45,
+            AttackModifier = 0,
+            AttackPerRound = 1,
+            Damage = "10d8",
+            DamageModifier = -2,
+            ArmorClass = 11
+        };
+
+        modelBuilder.Entity<Monster>().HasData(mimic, arasta, bugbearChief, agony);
+    }
 }
---
+++ DB/Migrations/20230114075931_CreateMonsters.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateMonsters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Monsters",
+                columns: new[] { "Id", "ArmorClass", "AttackModifier", "AttackPerRound", "Damage", "DamageModifier", "HitPoints", "Name" },
+                values: new object[,]
+                {
+                    { 1, 12, 18, 1, "9d8", 3, 58, "Mimic" },
+                    { 2, 19, 144, 1, "24d12", 7, 300, "Arasta" },
+                    { 3, 17, 20, 1, "10d8", 3, 65, "Bugbear Chief" },
+                    { 4, 11, 0, 1, "10d8", -2, 45, "Agony" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Monsters",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Monsters",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Monsters",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Monsters",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
