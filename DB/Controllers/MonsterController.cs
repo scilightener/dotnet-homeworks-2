f6393f421bc64a030f2cc29f39d3764c799aebba
scilightener <nikita.l.boss@gmail.com>
2023-01-14 11:09:47 +0300

    [add] db done

---
+++ DB/Controllers/MonsterController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DB.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MonsterController
+{
+    private readonly AppDbContext _db;
+    
+    public MonsterController(AppDbContext db) => _db = db;
+    
+    [HttpGet]
+    [Route("getRandom")]
+    public JsonResult GetRandom()
+    {
+        var total = _db.Monsters.Count();
+        var rnd = new Random();
+        var toSkip = rnd.Next(total);
+        return new JsonResult(_db.Monsters.Skip(toSkip).First());
+    }
+}
